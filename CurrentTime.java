f2caa4368b962a2fb492a4be47208239a60d1630
Nayan Bhile <nayan.bhile@tothenew.com>
2022-02-24 17:09:13 +0530

    Completed question 1

---
+++ CurrentTime.java
@@ -0,0 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CurrentTime {
+    static SimpleDateFormat sdf;
+
+    public static String getCurrentTime()
+    {
+        sdf=new SimpleDateFormat("HH:mm:ss:SS");
+        return String.valueOf(sdf.format(new Date().getTime()));
+    }
+    
+}
---
+++ Q1_2.java
@@ -0,0 +1,10 @@
+public class Q1_2 {
+    public static void main(String gg[]) throws InterruptedException
+    {   
+        Thread t1=new Thread( new q1Thread1());
+        Thread t2=new q1Thread2();
+        t1.start();
+        t1.join();
+        t2.start();
+    }
+}
